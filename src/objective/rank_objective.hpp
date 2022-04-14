313a7263ba819772e6fc5c1ac8d3bd1357c1ebec
CafebabeCoder <381336925@qq.com>
2022-04-14 10:59:46 +0800

    version-21 点击+意图=3 点击=2 意图=1 其他=0

--- src/objective/rank_objective.hpp
+++ src/objective/rank_objective.hpp
@@ -208,60 +208,56 @@ class LambdarankNDCG : public RankingObjective {
     }
     const double worst_score = score[sorted_idx[worst_idx]];
     double sum_lambdas = 0.0;
-    bool is_low_freq = qfreq[0] <= 0 ? true : false;
-    double delta_a = 0.0, delta_b = 0.0;
+    bool is_low_freq = qfreq[0] <= 3 ? true : false;
+    if(qfreq[0] == 0) {return;}
     // start accmulate lambdas by pairs that contain at least one document above truncation level
     for (data_size_t i = 0; i < cnt - 1 && i < truncation_level_; ++i) {
       if (score[sorted_idx[i]] == kMinScore) { continue; }
       for (data_size_t j = i + 1; j < cnt; ++j) {
         if (score[sorted_idx[j]] == kMinScore) { continue; }
-        data_size_t high_rank, low_rank;
-        if(is_low_freq){  //低频
-          continue;
-          delta_a = 0.0;
-          delta_b = 0.5;
+          label_t label_s_i, label_s_j;
+          if(label[sorted_idx[i]] ==1 && intent[sorted_idx[i]] > 0.7){
+            label_s_i = 3;
+          }else if(label[sorted_idx[i]] ==1){
+            label_s_i = 2;
+          }else if(intent[sorted_idx[i]]> 0.7){
+            label_s_i = 1;
+          }else{
+            label_s_i = 0;
+          }
+          if(label[sorted_idx[j]] ==1 && intent[sorted_idx[j]] > 0.7){
+            label_s_j = 3;
+          }else if(label[sorted_idx[j]] ==1){
+            label_s_j = 2;
+          }else if(intent[sorted_idx[j]]> 0.7){
+            label_s_j = 1;
+          }else{
+            label_s_j = 0;
+          }
           // skip pairs with the same labels
-          if((intent[sorted_idx[i]] < 0.7 && intent[sorted_idx[j]] < 0.7) || (intent[sorted_idx[i]] > 0.7 && intent[sorted_idx[j]] > 0.7)){continue;}
-          if(intent[sorted_idx[i]] > intent[sorted_idx[j]]){
+          if(label_s_i == label_s_j) {continue;}
+        data_size_t high_rank, low_rank;
+        int high_label, low_label;
+          if(label_s_i > label_s_j){
             high_rank = i;
             low_rank = j;
+            high_label = static_cast<int>(label_s_i);
+            low_label = static_cast<int>(label_s_j);
           }else{
             high_rank = j;
             low_rank = i;
+            high_label = static_cast<int>(label_s_j);
+            low_label = static_cast<int>(label_s_i);
           }
-        }else{ // 高频
-          delta_a = 1.0;
-          delta_b = 0.0;
-          // skip pairs with the same labels
-          if (label[sorted_idx[i]] == label[sorted_idx[j]]) { continue; }
-          if (label[sorted_idx[i]] > label[sorted_idx[j]]) {
-            high_rank = i;
-            low_rank = j;
-          } else {
-            high_rank = j;
-            low_rank = i;
-          }
-        }
         const data_size_t high = sorted_idx[high_rank];
-        const int high_label = static_cast<int>(label[high]);
         const double high_score = score[high];
         const double high_label_gain = label_gain_[high_label];
         const double high_discount = DCGCalculator::GetDiscount(high_rank);
         const data_size_t low = sorted_idx[low_rank];
-        const int low_label = static_cast<int>(label[low]);
         const double low_score = score[low];
         const double low_label_gain = label_gain_[low_label];
         const double low_discount = DCGCalculator::GetDiscount(low_rank);
 
-        double high_intent = intent[high] >= 0.7 ? intent[high] : 0.0;
-        double low_intent = intent[low] >= 0.7 ? intent[low] : 0.0;
-        double delta_intent = (high_intent - low_intent);
-        // delta_intent = 1.0 * delta_intent / (0.01 + fabs(max_intents_[query_id]));
-        // delta_intent = delta_intent > 0 ? std::min(1.0, delta_intent) : std::max(0.0, delta_intent);
-        // delta_intent = std::max(-1.0, delta_intent); 
-        // delta_intent = 0.5 * delta_intent / (0.01 + fabs(max_intents_[query_id])) + 0.5;
-        // delta_intent = GetSigmoid(delta_intent)-0.5;
-        // delta_intent = (delta_intent - min_intents_[query_id]) / (max_intents_[query_id] - min_intents_[query_id]);
         const double delta_score = high_score - low_score;
 
         // get dcg gap
@@ -269,7 +265,7 @@ class LambdarankNDCG : public RankingObjective {
         // get discount of this pair
         const double paired_discount = fabs(high_discount - low_discount);
         // get delta NDCG
-        double  delta_pair_NDCG = (delta_a * dcg_gap + delta_b * delta_intent) * paired_discount * inverse_max_dcg;
+        double  delta_pair_NDCG = dcg_gap * paired_discount * inverse_max_dcg;
         // if(is_low_freq)
           // delta_pair_NDCG = 0.5 * tmp_delta_ndcg;
 
@@ -290,8 +286,8 @@ class LambdarankNDCG : public RankingObjective {
         // lambda is negative, so use minus to accumulate
         sum_lambdas -= 2 * p_lambda;
         // Iteration:1
-        // Log::Info("[%d], qf=%1.0f, %d-%d, rank=%d/%d, label=%d/%d, intent=%.3f/%.3f, score=%.3f/%.3f, la=%.3f, he=%.3f, dPairNDCG=%.3f, clk/intent=%.3f/%.3f, a/b=%.1f/%.1f",
-          // query_id, qfreq[0], i, j, high_rank, low_rank, high_label, low_label, high_intent, low_intent, high_score, low_score, p_lambda, p_hessian, delta_pair_NDCG, delta_a * dcg_gap, delta_b * delta_intent, delta_a, delta_b);
+        // Log::Info("[%d], qf=%1.0f, %d-%d, rank=%d/%d, label=%d/%d, label=%d/%d, intent=%.3f/%.3f, score=%.3f/%.3f, la=%.3f, he=%.3f, dPairNDCG=%.3f",
+          // query_id, qfreq[0], i, j, high_rank, low_rank, static_cast<int>(label[high]), static_cast<int>(label[low]), high_label, low_label, intent[high], intent[low], high_score, low_score, p_lambda, p_hessian, delta_pair_NDCG);
       }
     }
     if (norm_ && sum_lambdas > 0) {
